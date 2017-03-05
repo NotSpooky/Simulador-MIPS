a67e1ee76b551593cef8e96bf24043f783e19cd3
Christian Durán <asistentedeprincipios2@gmail.com>
2017-03-04 21:53:59 -0600

    Elimina imports no utilizados

--- source/lectorarchivos.d
+++ source/lectorarchivos.d
@@ -2,14 +2,10 @@ module lectorarchivos;
 
 /// Retorna el espacio sobrante de posiciónMemoria.
 int [][] leerArchivo  (string nombreArchivo) {
-    import std.file;
     import std.stdio     : File;
     import std.array     : split, array, join;
-    import std.algorithm : map, each, copy;
+    import std.algorithm : map;
     import std.conv      : to;
-    import interpretador : Instrucción, Código;
-    
-    import std.stdio : writeln;
 
     auto archivo = File (nombreArchivo);
     return
