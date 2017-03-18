69e86069b8895a40caa572464e4cd54e97dcf8b8
Christian Durán <asistentedeprincipios2@gmail.com>
2017-03-17 21:29:31 -0600

    Vuelve imports en locales en app.d y elimina no usados

--- source/app.d
+++ source/app.d
@@ -1,23 +1,21 @@
-import std.stdio;
-import nucleo;
+import std.concurrency : spawn, Tid, thisTid;
 
-import std.concurrency;
 void main ()
 {
     import lectorarchivos;
-    import memorias : palabra, rellenarMemoria, bloqueInicioInstrucciones, memoriaPrincipal;
-    //palabra [64 * 4] memoria;
+    import memorias : rellenarMemoria, bloqueInicioInstrucciones, memoriaPrincipal;
     auto programa = `source/test.txt`.leerArchivo;
     rellenarMemoria (programa);
+    import std.stdio : writeln;
     writeln ("Memoria al iniciar ejecución: \n", memoriaPrincipal, '\n');
     auto tidNúcleo = spawn (&iniciarEjecución, thisTid, 0, bloqueInicioInstrucciones);
-    import std.typecons : Tuple;
-    import reloj        : iniciarReloj, HiloDeNúcleoConIdentificador;
+    import reloj : iniciarReloj, HiloDeNúcleoConIdentificador;
     iniciarReloj ([HiloDeNúcleoConIdentificador(tidNúcleo, 0)]);
 }
 
 // Tid son identificadores de cada hilo.
 void iniciarEjecución (Tid tidPadre, uint númeroNúcleo, uint contadorPrograma) {
+    import nucleo;
     Núcleo núcleo = new Núcleo (contadorPrograma, tidPadre, númeroNúcleo);
     núcleo.ejecutar;
 }
